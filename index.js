655abd18b85d8097571d5754b15b1f4227dc1777
fatumayussuf <fatumamaax2@gmail.com>
2024-10-21 21:29:47 +0300

    weather db

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+let app = express();
+let port = 4000;
+
+// let __fileName = fileURLToPath(import.meta.url);
+// console.log({ __fileName });
+
+// let __dirName = path.dirname(__fileName);
+// console.log({ __dirName });
+
+// Use static assets
+app.use(express.static("public"));
+
+// Define view templating engine
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// Home route
+app.get("/", async (req, res) => {
+  let query = req.query.q;
+  console.log({ query });
+
+  if (query) {
+    let cityRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`);
+
+    let city = await cityRes.json();
+
+    console.log({city});
+    //   Get location coordinates
+    let locationRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${process.env.OPENWEATHER_API_KEY}`
+    );
+
+    let location = await locationRes.json();
+
+    console.log({ location });
+
+    let { lon, lat } = location.coord;
+
+    //   Get weather info
+    let weatherRes = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`
+    );
+
+    let weather = await weatherRes.json();
+    console.log({ weather });
+
+    res.render("index", { weather });
+  }
+  
+  res.render("index");
+});
+
+app.listen(port, () => `Server started on port ${port}`);
